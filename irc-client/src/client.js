037bf5eabe25339b08d22a4fca40181c0f2a9f0f
Gabriel Miller <miller.gabrield@gmail.com>
2018-11-29 17:19:11 -0800

    Cleaning up, adding rooms

--- irc-client/src/client.js
+++ irc-client/src/client.js
@@ -10,7 +10,9 @@ var PORT = 6969;
 var client = new net.Socket();
 
 client.connect(PORT, HOST, () => {
+  //Log connection once extablished
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    //Prompt user for username (does not check if unique)
         prompt({
           title: 'Set username',
           label: 'Username:',
@@ -24,40 +26,41 @@ client.connect(PORT, HOST, () => {
             console.log('user cancelled');
           } else {
             console.log('result', r);
+            //Send username to server
             client.write("uname/" + r)
           }
         })
         .catch(console.error);
         var button = document.getElementById("sendButton");
         button.addEventListener("click",function(e){
-          var msg = "msg/0:0/" + document.getElementById("messageField").value;
+          //Sends message from text field to default room (0)
+          var msg = "msg/All/" + document.getElementById("messageField").value;
           client.write(msg);
+          //Reset value of input field
           document.getElementById("messageField").value = '';
         },false);
 });
-
+//Event handler for data received
 client.on('data', (data) => {
+      //Log data from server to console
       console.log('DATA: ' + data);
-      var recv = data.toString('utf8').split('/', 4);
+      //Parse response
+      var recv = data.toString('utf8').split('/');
       switch(recv[0]) {
+        //If server sends uname, a new user has joined
+        //format: opcode/username
         case 'uname':
-          var newUser = recv[1];
           console.log(newUser + " just joined the room");
           var temp = document.getElementById("thread").insertRow(-1);
-          temp.innerHTML = newUser + " just joined!";
+          temp.innerHTML = recv[1] + " just joined!";
           break;
+        //If server sends msg, a message has been relayed
+        //The server should handle sending messages to the correct room this
+        //client is in
+        //format: opcode/user/(room(s))/message content
         case 'msg':
-          var message = recv[3];
           var temp = document.getElementById("thread").insertRow(-1);
-          temp.innerHTML = recv[1] + ": " + message;
-          if(recv[1] === userName)
-            {
-              console.log("You to " + message[1] + ": " + message[3]);
-            }
-          else
-            {
-              console.log(message[0] + " to " + message[1] + ":" + message[3]);
-            }
+          temp.innerHTML = recv[1] + "(" + recv[2] + "): " + recv[3];
           break;
         case 'left':
           var leaveMsg = recv[1].split('/', 2);
--- irc-server/serv.js
+++ irc-server/serv.js
@@ -4,6 +4,10 @@ var HOST = '127.0.0.1';
 var PORT = 6969;
 
 var clientArray = [];
+var room = {
+  name: '',
+  members: [],
+}
 
 function relay(msg) {
   for(i = 0; i < clientArray.length; i++)
@@ -16,15 +20,14 @@ function relay(msg) {
 // The function passed to net.createServer() becomes the event handler for the 'connection' event
 // The sock object the callback function receives UNIQUE for each connection
 net.createServer( function(socket) {
-
-    //Print
     console.log('CONNECTED: ' + socket.remoteAddress +':'+ socket.remotePort);
-    // Add a 'data' event handler to this instance of socket
 
     socket.on('data', function(data) {
-        var recv = data.toString('utf8').split('/', 3)
-        console.log(recv);
+        var recv = data.toString('utf8').split('/')
         switch(recv[0]) {
+          //If message begins with /uname, a new client is joining.
+          //Push client to array, send a welcome message, then broadcast
+          //new user connection to all clients
           case 'uname':
             socket.name = recv[1];
             clientArray.push(socket);
@@ -32,10 +35,11 @@ net.createServer( function(socket) {
             var toSend = "uname/" + socket.name;
             relay(toSend);
             break;
+            //If message begins with msg, it is from a client. Relay to others.
           case 'msg':
-            var message = recv[2] //message[0] will be room number
+            var message = recv[2]
             console.log(message);
-            var toSend = "msg/" + socket.name + "/" + "0:0/" + message;
+            var toSend = "msg/" + socket.name + "/" + "All/" + message;
             console.log(socket.name + ": " + message);
             relay(toSend);
             break;
@@ -45,6 +49,12 @@ net.createServer( function(socket) {
     // Handles client closing connection
     socket.on('close', function(data) {
         console.log('CLOSED: ' + socket.remoteAddress +' '+ socket.remotePort);
+        //Don't want to relay messages to closed connections
+        for( var i = 0; i < clientArray.length-1; i++){
+          if ( clientArray[i].name === socket.name) {
+            clientArray.splice(i, 1);
+          }
+        }
     });
 
 }).listen(PORT, HOST);
