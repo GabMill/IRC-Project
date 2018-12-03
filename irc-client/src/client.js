c5471e7761085e8b906ba46b8bcc792b631f2df8
Gabriel Miller <miller.gabrield@gmail.com>
2018-12-03 15:56:50 -0800

    Added comments per TA request
    After the demo, the TA requested that comments be added to source code to ease readability for the professor. No functionality has changed, just comments added.

--- irc-client/src/client.js
+++ irc-client/src/client.js
@@ -1,28 +1,37 @@
 const electron = require('electron');
-
 const net = require('net');
-
-let promptWindow = null;
+//Set host address (local) and port number to connect to
 var HOST = '127.0.0.1';
 var PORT = 8080;
-
+//Create new socket object
 var client = new net.Socket();
-
+//Connect to server
 client.connect(PORT, HOST, () => {
+  //Logs connection to console
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-
+        //Handle user input
         var button = document.getElementById("sendButton");
         button.addEventListener("click",function(e){
           var input = document.getElementById("messageField").value
+          //Split user input with '/' delimiter to process and send to server
           msg = input.split('/');
+          //Create variable to hold string to send to server
           var toSend;
+          //First section of the string entered by user should contain
+          //a command - this switch statement processes that command to determine
+          //what needs to be sent to the server
           switch(msg[0]) {
+          //list command allows user to list rooms or members of rooms
+          //Usage: list/rooms for room listing, list/users/(roomname) for users in room
             case 'list':
+            //If user is listing rooms, create command to send to server
               if(msg[1] === 'rooms')
               {
                 toSend = "list/rooms"
                 break;
               }
+              //If user is listing rooms, create command to send to server
+              //"msg[2]" will be the room the user wants to list members of
               else if(msg[1] === 'users')
               {
                 toSend = "list/users/" + msg[2];
@@ -32,51 +41,85 @@ client.connect(PORT, HOST, () => {
                 console.log("Bad list input");
                 break;
               }
+            //User is setting username
+            //Usage: uname/(username)
             case 'uname':
               toSend = input;
               break;
+            //User is creating a new room
+            //Usage: new/(roomname)
             case 'new':
               toSend = input;
               break;
+            //User is joining a room
+            //Usage: join/(roomname)
             case 'join':
               toSend = input;
               break;
+            //User is leaving a room
+            //Usage: leave/(roomname)
             case 'leave':
               toSend = input;
               break;
+            //User is sending a message to a specific room(s)
+            //Usage: room/roomname/(message)
+            //A user may send to multiple rooms seperated by colons
+            //Ex: room/Room1:Room2:Room3/message
             case 'room':
               toSend = input;
               break;
+            //User is sending message to another user
+            //Usage: dm/(user)/message
             case 'dm':
               toSend = input;
               break;
+            //Default behavior - if user enters string that doesn't contain a
+            //specific command, it is sent to the server as a message that
+            //will be relayed to the "All" room
             default:
               toSend = "msg/" + input
               break;
           }
+          //Send string to the server once input has been processed and formatted
+          //for server
           client.write(toSend);
           document.getElementById("messageField").value = '';
           e.preventDefault();
         },false);
 });
+
+//Handler for data received from server
 client.on('data', (data) => {
+  //Log data to console for debugging
       console.log('DATA: ' + data);
+      //Split string by '/' delimiter and switch
       var recv = data.toString('utf8').split('/');
       switch(recv[0]) {
+        //uname command from server indicates another client has connected.
+        //The second part of the string will be the name of the new user
         case 'uname':
           console.log(recv[1] + " just joined the room");
+          //Insert message into table (I know this is a really awful way to
+          //do this but I was crunched on time)
           var temp = document.getElementById("thread").insertRow(-1);
           temp.innerHTML = recv[1] + " just joined!";
           break;
+        //msg command from server indicates the client is receiving a message
+        //from a room they have joined or another user
         case 'msg':
           var temp = document.getElementById("thread").insertRow(-1);
           temp.innerHTML = recv[1] + "(" + recv[2] + "): " + recv[3];
           break;
+        //left command from server indicates a user has disconnected from the
+        //server - log to chat window
         case 'left':
           var temp = document.getElementById("thread").insertRow(-1);
           temp.innerHTML = recv[1] + " just left";
           console.log(recv[1] + " just left");
           break;
+        //list command from server indicates user requested to list rooms or
+        //users in a specific room. The response from the server is logged to the
+        //chat window
         case 'list':
           var list = recv[2].split(':');
           if(recv[1] === 'rooms'){
@@ -96,5 +139,8 @@ client.on('data', (data) => {
   });
 
 client.on('close',() => {
+  //This handles the connection closing when the server quits/crashes.
+  var temp = document.getElementById("thread").insertRow(-1);
+  temp.innerHTML = "Connection to server lost (server likely crashed)";
   console.log('Connection closed');
 });
--- irc-server/serv.js
+++ irc-server/serv.js
@@ -1,11 +1,20 @@
 var net = require('net');
-
+//Set host address (local) and port number to connect to
 var HOST = '127.0.0.1';
 var PORT = 8080;
+
+//Arrays for keeping trac of connected clients and rooms
 var clientArray = [];
 var roomArray = [];
+
+//Boolean for overriding relay at the end of switch statement
+//so that roomRelay and userRelay can be used and messages
+//aren't relayed to users that shouldn't be getting them
 var dorelay = true;
 
+//This function looks through the list of rooms for the room the
+//server needs to relay a message to so that users don't receive messages
+//for rooms they have not joined
 function roomRelay(msg, room) {
   if(clientArray.length === 0) {
     console.log("No connected clients")
@@ -21,6 +30,7 @@ function roomRelay(msg, room) {
   }
 }
 
+//This function is for relaying direct messages to the user they are intended for
 function userRelay(msg, user) {
   if(clientArray.length === 0) {
     console.log("No connected clients")
@@ -34,6 +44,9 @@ function userRelay(msg, user) {
   }
 }
 
+//This function relays a message to all users. It is used for sending to the
+//"All" channel and for sending other commands that all users should receive
+//(such as join/leave notification messages)
 function relay(msg) {
   if(clientArray.length === 0)
   {
@@ -47,30 +60,50 @@ function relay(msg) {
   }
 }
 
+//The net.createServer method sets up a server that listens on a specified
+//host and port number and provides a "socket" object that can be written to
+//and read from to relay messages
 net.createServer( function(socket) {
+    //Log connection when established
     console.log('CONNECTED: ' + socket.remoteAddress +':'+ socket.remotePort);
 
+    //Room object for storing users in a room and the rooms name
     function Room(name, user) {
       this.name = '';
       this.name = name;
       this.users = [];
       this.users.push(user);
     }
-
+    //Handler for data received from a client
     socket.on('data', function(data) {
+        //Split data using '/' delimiter
         var recv = data.toString('utf8').split('/')
+        //Logs data (really just for debugging)
         console.log(recv);
+        //Create variable to hold string to relay to client(s)
         var toSend;
+        //Check if the "All" room has been set up. If it hasn't, this is the
+        //first client to connect so the room needs to be created.
         if (!roomArray[0]) {
           var newRoom = new Room('All', socket)
           roomArray.push(newRoom);
           console.log(newRoom.name)
         }
+        //Start switch statement to process data received
         switch(recv[0]) {
+          //uname command means a user is setting their username
           case 'uname':
+            //Add this socket to the list of connected clients
             clientArray.push(socket)
+            //Set the sockets "name" to the name set by the client
             socket.name = recv[1];
+            //Create string to relay to clients to inform them that a new user
+            //has connected
             toSend = "uname/" + socket.name;
+            //If for some reason the "All" room doesn't exist or the room array
+            //is empty, create it and add this socket to its list of members.
+            //Otherwise, add this socket to the "All" channel (which should always
+            //be index[0])
             if (!roomArray[0]) {
               var newRoom = new Room('All', socket)
               roomArray.push(newRoom);
@@ -80,35 +113,60 @@ net.createServer( function(socket) {
               roomArray[0].users.push(socket);
             }
             break;
+          //msg command indicates user is sending message to a channel
           case 'msg':
+            //If user has not set username and is trying to send a message,
+            //set a default username
             if(!socket.name)
             {
               socket.name = "unameNotSet";
             }
+            //If for some reason the "All" room doesn't exist or the room array
+            //is empty, create it and add this socket to its list of members.
+            //Otherwise, add this socket to the "All" channel (which should always
+            //be index[0])
             if (!roomArray[0]) {
               var newRoom = new Room('All', socket)
               roomArray.push(newRoom);
               console.log(newRoom.name)
             }
+            //Create response string to send to clients
             toSend = "msg/" + socket.name + "/" + "All/" + recv[1];
             console.log(socket.name + ": " + recv[1]);
             break;
+          //list command indicates client is requesting list of rooms or users
+          //in a specific room
           case 'list':
+            //Set doRelay to false so standard relay function is not called
+            //after switch ends
             dorelay = false;
+            //If for some reason the "All" room doesn't exist or the room array
+            //is empty, create it and add this socket to its list of members.
+            //Otherwise, add this socket to the "All" channel (which should always
+            //be index[0])
             if (!roomArray[0]) {
               var newRoom = new Room('All', socket)
               roomArray.push(newRoom);
               console.log(newRoom.name)
             }
+            //room command indicates client wishes to list rooms
             if(recv[1] === 'rooms'){
+              //set command section of string to relay to client
               toSend = "list/rooms/"
-              console.log(roomArray.length);
+              console.log(roomArray.length);//for debugging
               console.log(roomArray[0].name)
+              //Loop through room array and get room names. Delimited by ':'
               for(i = 0; i < roomArray.length; i++){
                 toSend = toSend + roomArray[i].name + ':';
               }
+            //users command indicates client wishes to list users in specific
+            //room
             }else{
+              //set command section of string to relay to client
               toSend = "list/users/";
+              //Loop through room array until the requested room is found.
+              //If found, users of that room are added to response string and
+              //delimited by ':'
               for(i = 0; i < roomArray.length; i++){
                   if(roomArray[i].name === recv[2]){
                   for(j = 0; j < roomArray[i].users.length; j++){
@@ -117,14 +175,21 @@ net.createServer( function(socket) {
                 }
               }
             }
+            //Send data to client that requested listing
             socket.write(toSend + "/" + recv[2])
             break;
+          //new command indicates user is creating a new room
           case 'new':
+            //Add new room to room array using name provided by client and add
+            //that client to that room
             var newRoom = new Room(recv[1], socket)
             roomArray.push(newRoom);
+            //Create string to send to clients to notify them of new room
             toSend = "msg/" + socket.name + "/" + "All/Created new room: " + recv[1];
             break;
+          //join command indicates client is trying to join a new room
           case 'join':
+            //Don't want to relay anything, just add client to room
             dorelay = false;
             for(i = 0; i < roomArray.length; i++){
                 if(roomArray[i].name === recv[1]){
@@ -132,7 +197,9 @@ net.createServer( function(socket) {
               }
             }
             break;
+          //leave command indicates client wishes to leave a specific room
           case 'leave':
+            //Don't want to relay anything, just remove from room
             dorelay = false;
             for(i = 0; i < roomArray.length; i++){
               if(roomArray[i].name === recv[1]){
@@ -140,34 +207,53 @@ net.createServer( function(socket) {
               }
             }
             break;
+          //room command indicates client wishes to send a message to a
+          //specific room
           case 'room':
+            //Don't want to use standard relay function as it will send to
+            //clients not in the desired room
             dorelay = false;
+            //Create string to relay to clients
             toSend = "msg/" + socket.name + "/" + recv[1] + "/" + recv[2];
+            //Relay message to clients in room specified by user (in recv[1])
             roomRelay(toSend, recv[1])
             break;
+          //dm command indicates client wishes to send a message to a specific
+          //client
           case 'dm':
+            //Don't want to use standard relay function as it will send to
+            //all clients and not the specified client
             dorelay = false;
+            //Create string to relay to client
             toSend = "msg/" + socket.name + "/dm/" + recv[2];
+            //Relay message to specified client
             userRelay(toSend, recv[1])
             break;
         }
+      //Check if standard relay function needs to be used
       if(dorelay){
+        //Relay message to all clients
         relay(toSend);
       }
+      //Reset dorelay if it had been set to false
       else{
         dorelay = true;
       }
     });
-
+    //Handles connection closing
     socket.on('close', function(data) {
         console.log('CLOSED: ' + socket.remoteAddress +' '+ socket.remotePort);
+        //Loop through client list and remove client from array so that
+        //server won't attempt to send to clients that have disconnected
         for( var i = 0; i < clientArray.length; i++){
           if ( clientArray[i] === socket) {
             clientArray.splice(i, 1);
           }
         }
+        //Relay message to all clients that a user has disconnected
         relay("left/" + socket.name)
     });
+//Port and host that the server should listen on are specified here
 }).listen(PORT, HOST);
 
 console.log('Server listening on ' + HOST +':'+ PORT);
